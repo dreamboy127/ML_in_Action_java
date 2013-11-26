fe98bb2f474c629a48896158df57609bfcfbbc84
dreamboy127 <jerrychou127@gmail.com>
2013-11-26 12:06:23 +0800

    ML

---
+++ KNN/hello.java
@@ -0,0 +1,11 @@
+/*************************************************************************
+	> File Name: hello.java
+	> Author: jerrychou
+	> Mail: jerrychou127@gmail.com 
+	> Created Time: Tue 26 Nov 2013 11:41:01 AM CST
+ ************************************************************************/
+public class hello{
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+    }
+}
